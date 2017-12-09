28a5ee7a547cd5c83e36a9939fa61ea732cc73d9
mscott935 <mscott935.ms@gmail.com>
2017-12-09 14:58:18 -0500

    addBuilding functionality added

--- harvard.py
+++ harvard.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-#coding: utf-8
 
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -7,26 +6,37 @@ from mininet.util import irange,dumpNodeConnections
 from mininet.log import setLogLevel
 
 class HarvardTopo(Topo):
-	'''Create topology that mimics some Harvard locations and buildings, including Leverett House, McKinlock Hall
-		(a part of Leverett House), Maxwell Dworkin, and Pierce Hall. Most infrastructure abstracted away.'''
-	
-	def build(self, **opts):
-		switch = self.addSwitch('Pre-Controller')
-		mckin = self.addHost('McKinlock')
-		lev = self.addHost('Leverett House')
-		maxwell = self.addHost('G Tower')
-		pierce = self.addHost('Pierce')
+    '''Create topology that mimics some Harvard locations and buildings, including Leverett House,
+        Maxwell Dworkin, and Pierce Hall. Most infrastructure abstracted away.'''
 
-		self.addLink(switch, lev)
-		self.addLink(lev, mckin)
-		self.addLink(switch, maxwell)
-		self.addLink(switch, pierce)
+    def __init__(self, **opts):
 
-	def addBuilding(self, host):
-		self.addLink(switch, host)
+        Topo.__init__(self, **opts)
+
+        self.switch = self.addSwitch('s1')
+        lev = self.addHost('h1')
+        maxwell = self.addHost('h2')
+        pierce = self.addHost('h3')
+
+        self.addLink(self.switch, lev)
+        self.addLink(self.switch, maxwell)
+        self.addLink(self.switch, pierce)
+
+    def addBuilding(self, name, **opts):
+        host = self.addHost(name)
+        self.addLink(self.switch, host, **opts)
+
+    '''def removeBuilding(self, name):
+        self.delLinkBetween(switch, host)
+        self.delnode(host, nodes = self.hosts)
+
+    def updateLink(self, host, linkopts):
+        self.delLinkBetween(switch, host)
+        self.addLink(switch, host, **linkopts)'''
+
 
 def simpleTest():
-	"Create and test a simple network"
+    "Create and test a simple network"
     topo = HarvardTopo()
     net = Mininet(topo)
     net.start()
@@ -36,7 +46,19 @@ def simpleTest():
     net.pingAll()
     net.stop()
 
+def addTest():
+    topo = HarvardTopo()
+    topo.addBuilding("Leverett")
+    net = Mininet(topo)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    net.stop()
+
 if __name__ == '__main__':
-	# Tell mininet to print useful information
-	setLogLevel('info')
-	simpleTest()
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
+    addTest()
