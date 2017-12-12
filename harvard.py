c2f69b89ff2a207ebf1291e8e26cd4385e7655ed
mscott935 <mscott935.ms@gmail.com>
2017-12-12 14:03:58 -0500

    v1.0.0 complete

--- harvard.py
+++ harvard.py
@@ -13,11 +13,11 @@ class HarvardTopo(Topo):
         Maxwell Dworkin, and Pierce Hall. Most infrastructure abstracted away.'''
 
     def build(self):
-        "Init."
+        "Init"
         # Buildings are stored in a dict in the form {Name: (host, profile)}
         self.buildings = {}
-        # Initialize switches and default hosts. Switch is an instance variable for reference in other methods
-        self.switch = self.addSwitch('s1')
+        # Initialize switches and default hosts.
+        switch = self.addSwitch('s1')
         lev = self.addHost('h1')
         maxwell = self.addHost('h2')
         pierce = self.addHost('h3')
@@ -26,18 +26,18 @@ class HarvardTopo(Topo):
         self.buildings['Maxwell Dworkin'] = (maxwell, "S")
         self.buildings['Pierce'] = (pierce, "S")
 
-        self.addLink(self.switch, lev)
-        self.addLink(self.switch, maxwell)
-        self.addLink(self.switch, pierce)
+        self.addLink(switch, lev)
+        self.addLink(switch, maxwell)
+        self.addLink(switch, pierce)
 
     def getBuildings(self):
         return self.buildings
 
-def updateLinks(profile, net, topo):
-    "Update link bandwidth based on time of day and building profile."
-    time = datetime.datetime.now()
+def updateLinks(profile, net, topo, time=datetime.datetime.now()):
+    "Update link bandwidth based on time of day and building profile"
     switch = net.get('s1')
     buildings = topo.getBuildings()
+    print "Updating " + profile + "-type host links..."
     if profile == "R":
         # Gather all residential hosts
         resHosts = [buildings[x][0] for x in buildings if buildings[x][1] == "R"]
@@ -52,9 +52,8 @@ def updateLinks(profile, net, topo):
             elif time.hour >= 17 or time.hour < 5:
                 srcLink.config(**{'bw' : 500, 'loss' : 0})
                 dstLink.config(**{'bw' : 500, 'loss' : 10})
-                print("Time 17-5 done!")
             else:
-                print("Unsupported link update time at time" + time.hour + "!")
+                print "Unsupported link update time at time " + time.hour + "!"
     elif profile == "S":
         sciHosts = [buildings[x][0] for x in buildings if buildings[x][1] == "S"]
         for hostname in sciHosts:
@@ -75,48 +74,10 @@ def updateLinks(profile, net, topo):
                 srcLink.config(**{'bw' : 100, 'loss' : 0})
                 dstLink.config(**{'bw' : 100, 'loss' : 0})
             else:
-                print("Unsupported link update time at time" + time.hour + "!")
+                print "Unsupported link update time at time " + time.hour + "!"
     else:
-        print("Unsupported building profile" + profile + "!")
-
-'''def updateLinks(profile, net, topo):
-    "Update link bandwidth based on time of day and building profile."
-    time = datetime.datetime.now()
-    switch = net.get('s1')
-    buildings = topo.getBuildings()
-    if profile == "R":
-        # Gather all residential hosts
-        resHosts = [buildings[x][0] for x in buildings if buildings[x][1] == "R"]
-        for hostname in resHosts:
-            host = net.get(hostname)
-            links = host.connectionsTo(switch)
-            intf = host.intf()
-            if time.hour >= 5 and time.hour < 17:
-                intf.config(bw = 350)
-            elif time.hour >= 17 or time.hour < 5:
-                intf.config(bw = 500)
-                print("Time 17-5 done!")
-            else:
-                print("Unsupported link update time at time" + time.hour + "!")
-    elif profile == "S":
-        sciHosts = [buildings[x][0] for x in buildings if buildings[x][1] == "S"]
-        for hostname in sciHosts:
-            host = net.get(hostname)
-            links = host.connectionsTo(switch)
-            srcLink = links[0][1]
-            dstLink = links[0][1]
-            if time.hour >= 5 and time.hour < 13:
-                intf.config(bw = 500)
-            elif time.hour >= 13 and time.hour < 17:
-                intf.config(bw = 500)
-            elif time.hour >= 17 and time.hour < 22:
-                intf.config(bw = 500)
-            elif time.hour >= 22 or time.hour < 5:
-                intf.config(bw = 500)
-            else:
-                print("Unsupported link update time at time" + time.hour + "!")
-    else:
-        print("Unsupported building profile" + profile + "!")'''
+        print "Unsupported building profile " + profile + "!"
+    print profile + "-Type building links updated!"
 
 def runNetwork():
     topo = HarvardTopo()
@@ -132,10 +93,46 @@ def runNetwork():
     schedule.every().day.at("22:00").do(updateLinks, "S", net, topo)
     CLI(net)
 
+def updateTest():
+    midnight = datetime.datetime(2017, 1, 1, 0, 0, 0) # 00:00 Jan 1, 2017
+    fiveam = datetime.datetime(2017, 1, 1, 5, 0, 0) # 05:00 Jan 1, 2017
+    onepm = datetime.datetime(2017, 1, 1, 13, 0, 0) # 13:00 Jan 1, 2017
+    fivepm = datetime.datetime(2017, 1, 1, 17, 0, 0) # 17:00 Jan 1, 2017
+    topo = HarvardTopo()
+    net = Mininet(topo, link=TCLink)
+    net.start()
+
+    # Midnight test
+    print "*** EXECUTING MIDNIGHT TEST ***"
+    updateLinks("R", net, topo, midnight)
+    updateLinks("S", net, topo, midnight)
+    net.iperf()
+    time.sleep(5)
+
+    # 5am test
+    print "*** EXECUTING 5AM TEST ***"
+    updateLinks("R", net, topo, fiveam)
+    updateLinks("S", net, topo, fiveam)
+    net.iperf()
+    time.sleep(5)
+
+    # 1pm test
+    print "*** EXECUTING 1PM TEST ***"
+    updateLinks("R", net, topo, onepm)
+    updateLinks("S", net, topo, onepm)
+    net.iperf()
+    time.sleep(5)
+
+    # 5pm test
+    print "*** EXECUTING 5PM TEST ***"
+    updateLinks("R", net, topo, fivepm)
+    updateLinks("S", net, topo, fivepm)
+    net.iperf()
+    time.sleep(5)
+
+    net.stop()
+
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
     runNetwork()
-
-# {'Leverett': ('h1', 'R'), 'Pierce': ('h3', 'S'), 'Maxwell Dworkin': ('h2', 'S')}
-# [ buildings[x][0] if ]
